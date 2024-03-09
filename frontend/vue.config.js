4fa87625011877ac968e3d9067c6c0102f79bc88
lukasstingl <lukas.stingl@gmail.com>
2024-03-09 09:19:56 +0100

    add dist dest

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  outputDir: path.resolve(__dirname, '../../var/www/html/dist'),
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
