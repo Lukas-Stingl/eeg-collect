f59a54e48ce7bdb18947f6e30954bc1bbafc2877
lukasstingl <lukas.stingl@gmail.com>
2024-03-09 09:42:03 +0100

    changed dest folder

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: path.resolve(__dirname, '../../../var/www/html/dist'),
+  outputDir: path.resolve(__dirname, '../../../var/www/html'),
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
