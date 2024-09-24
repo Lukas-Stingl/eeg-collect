945b5aee6ecaeec0a99d5c41c3f4ee180e538421
lukasstingl <lukas.stingl@gmail.com>
2024-09-24 16:49:34 +0200

    missed bracket

--- frontend/src/scripts/cyton.js
+++ frontend/src/scripts/cyton.js
@@ -554,7 +554,7 @@ export class cyton {
       }
     }
   }
-}
+
     logErrorDetails(error, buffer) {
       console.log("Error occurred:", error.message);
       console.log("Stack trace:", error.stack);
