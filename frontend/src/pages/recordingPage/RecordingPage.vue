a9c8251e6208888fa64eb832c125b8d40d937f42
Felix Wang <felix-wang@outlook.de>
2024-11-09 15:55:30 +0100

    setup chart library for data visualization

--- frontend/src/pages/recordingPage/RecordingPage.vue
+++ frontend/src/pages/recordingPage/RecordingPage.vue
@@ -1,15 +1,68 @@
 <script setup lang="ts">
+import { Chart } from "highcharts-vue";
+
 import BasePage from "@/components/BasePage.vue";
 import {
   useConfigureParticipantId,
   useOpenBCIUtils,
   useWebsocketConnection,
 } from "@/utils/hooks";
+import { computed, ComputedRef, onMounted, Ref, ref } from "vue";
+import { Options } from "highcharts";
 
 // ---- STATE ----
 useConfigureParticipantId();
 useWebsocketConnection();
 const { startSignalQualityCheck, stopRecording, signalRMS } = useOpenBCIUtils();
+
+const series = ref<{}[]>([]);
+const data = ref();
+const seriesCount = ref(8);
+const pointsCount = ref(100);
+const axisTop = ref(20);
+const range = ref();
+const totalHeight = 500;
+const yAxisHeight = computed(() => totalHeight / (seriesCount.value + 7));
+const yAxis = ref<{}[]>([]);
+
+const chartOptions: ComputedRef<Options> = computed(() => ({
+  chart: {
+    height: totalHeight,
+    //spacing: [0, 0, 0, 0], // Remove padding
+  },
+  title: null,
+  series: series.value,
+  yAxis: yAxis.value,
+}));
+
+onMounted(() => {
+  for (let i = 0; i < seriesCount.value; i++) {
+    range.value = Math.round(Math.random() * 100);
+    data.value = [];
+    for (let j = 0; j < pointsCount.value; j++) {
+      data.value.push(Math.floor(Math.random() * range.value));
+    }
+    series.value.push({
+      data: data.value,
+      yAxis: i,
+    });
+    yAxis.value.push({
+      title: {
+        text: `A${i + 1}`,
+      },
+      labels: {
+        enabled: false, // Remove Y-axis labels
+      },
+      height: yAxisHeight.value,
+      top: axisTop.value,
+      offset: 0,
+      lineWidth: 0, // Remove Y-axis line
+      gridLineWidth: 0, // Remove grid lines
+    });
+
+    axisTop.value += yAxisHeight.value + 12.5;
+  }
+});
 </script>
 
 <template>
@@ -24,5 +77,14 @@ const { startSignalQualityCheck, stopRecording, signalRMS } = useOpenBCIUtils();
     >
       Recording in progress. You can now return to the experiment Tab.<br />
     </p>
+
+    <Chart :options="chartOptions" class="mb-8"></Chart>
+
+    <v-btn @click="() => {}">Stop Recording</v-btn>
   </BasePage>
 </template>
+<style>
+.highcharts-credits {
+  display: none;
+}
+</style>
