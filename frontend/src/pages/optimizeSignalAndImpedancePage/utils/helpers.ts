804d8cdfc0f55599d5e49ddab14caa27714985d2
Felix Wang <felix-wang@outlook.de>
2024-11-12 22:27:51 +0100

    adjusted impedance rms thresholds

--- frontend/src/pages/optimizeSignalAndImpedancePage/utils/helpers.ts
+++ frontend/src/pages/optimizeSignalAndImpedancePage/utils/helpers.ts
@@ -5,11 +5,11 @@ export const getSignalState = ({ signal }: { signal?: number }) => {
 
   // TODO I think thresholds and values are different for each node
 
-  if (signal < 30000) {
-    return 3;
-  } else if (signal < 80000) {
+  if (signal > 0.9) {
+    return 1;
+  } else if (signal > 0.6) {
     return 2;
   } else {
-    return 1;
+    return 3;
   }
 };
