d2112ca16aeef7bd2c108523038e6e70662082e7
Felix Wang <felix-wang@outlook.de>
2025-06-15 15:07:05 +0200

    adjust RMS threshold

--- frontend/src/pages/optimizeSignalAndImpedancePage/utils/helpers.ts
+++ frontend/src/pages/optimizeSignalAndImpedancePage/utils/helpers.ts
@@ -1,9 +1,9 @@
 export const getSignalState = ({ signal }: { signal?: number }) => {
   if (!signal || signal < 0.5) {
     return 1;
-  } else if (signal > 300) {
+  } else if (signal > 125) {
     return 2;
-  } else if (signal > 100) {
+  } else if (signal > 75) {
     return 3;
   } else {
     return 4;
